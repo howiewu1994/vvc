b8d68e3f1757d5d5938031c0c9a8056c963a4650
howiewu1994 <10122510136@ecnu.cn>
2017-03-21 15:10:00 +0800

    changname
    correct the name

--- src/Model/Data/User.php
+++ src/Model/Data/User.php
@@ -43,7 +43,7 @@ class User
 
     public function setUsername(string $username)
     {
-    	$this->use_rname = $username;
+    	$this->user_name = $username;
     	//$this->username = $username;
     }
 
